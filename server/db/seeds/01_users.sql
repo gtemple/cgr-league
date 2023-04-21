6f4fd97a6af769647afde019ce528a9c141cb327
Giordano Temple <giordanotemple@gmail.com>
2023-04-21 10:33:35 -0400

    add development seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (id, email, password) VALUES (1, 'rick.sandchez@gmail.com', 'picklerick');
-INSERT INTO users (id, email, password) VALUES (2, 'lisa.simpson@gmail.com', 'ehhhhh');
-INSERT INTO users (id, email, password) VALUES (3, 'link@yahoo.com', 'hyrule');
-INSERT INTO users (id, email, password) VALUES (4, 'simon_bel123@mail.ca', 'dracula');
-INSERT INTO users (id, email, password) VALUES (5, 'all_might@academia.jp', 'plusUltra');
-INSERT INTO users (id, email, password) VALUES (6, 'mario@mushroomkindom.jp', 'plumber79');
+INSERT INTO users (id, first_name, last_name, city_of_birth, country_of_birth, date_of_birth) VALUES (1, 'Giordano', 'Temple', 'Toronto', 'Canada', '1993-03-11');
+INSERT INTO users (id, first_name, last_name, city_of_birth, country_of_birth, date_of_birth) VALUES (2, 'Cole', 'Reynolds', 'Toronto', 'Canada', '1996-10-10');
+INSERT INTO users (id, first_name, last_name, city_of_birth, country_of_birth, date_of_birth) VALUES (3, 'Ryan', 'Reynolds', 'Toronto', 'Canada', '1992-12-05');
+INSERT INTO users (id, first_name, last_name, city_of_birth, country_of_birth, date_of_birth) VALUES (4, 'Charles', 'Leclerc', 'Monte Carlo', 'Monaco', '1997-10-16');
+INSERT INTO users (id, first_name, last_name, city_of_birth, country_of_birth, date_of_birth) VALUES (5, 'Lando', 'Norris', 'Bristol', 'United Kingdom', '1999-11-13');
+INSERT INTO users (id, first_name, last_name, city_of_birth, country_of_birth, date_of_birth) VALUES (6, 'Fernando', 'Alonso', 'Oviedo', 'Spain', '1981-07-29');
---
+++ server/db/seeds/02_tracks.sql
@@ -0,0 +1,3 @@
+-- seeds/02_urls.sql\
+INSERT INTO tracks (id, name, city, country, distance) VALUES (1, 'Azerbaijan Grand Prix', 'Baku', 'Azerbaijan', 6003);
+INSERT INTO tracks (id, name, city, country, distance) VALUES (2, 'British Grand Prix', 'Silverstone', 'United Kingdom', 5861);
