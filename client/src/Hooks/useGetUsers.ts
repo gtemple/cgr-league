aa82d792c92460ea512abdb4ee8946872fee182e
Giordano Temple <giordanotemple@gmail.com>
2023-05-13 09:37:06 -0400

    fix users type

--- client/src/Hooks/useGetUsers.ts
+++ client/src/Hooks/useGetUsers.ts
@@ -3,9 +3,22 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(import.meta.env.VITE_DB_URL, import.meta.env.VITE_DB_KEY);
 
+type User = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  initials: string;
+  city_of_birth: string;
+  country_of_birth: string;
+  country_of_representation: string;
+  date_of_birth: string;
+  human: boolean;
+  profile_image: string;
+};
+
 export default function useGetUsers() {
-  const [userData, setUserData] = useState<array>([])
-  const [loaded, setLoaded] = useState<boolean>(false)
+  const [userData, setUserData] = useState<User[]>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     getUsers();
@@ -18,5 +31,5 @@ export default function useGetUsers() {
 
   return {
     userData
-  }
+  };
 }
