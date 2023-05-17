e6d9efe67ca1edecec3a4501a634d12a4114250d
Giordano Temple <giordanotemple@gmail.com>
2023-05-17 12:39:34 -0400

    add tables

--- client/src/components/Tracks/AllTime.tsx
+++ client/src/components/Tracks/AllTime.tsx
@@ -1,43 +1,105 @@
 import { positionScore } from "../../helpers/sumSeasonPoints";
 
-    //@ts-expect-error
+interface TrackData {
+  created_at: string;
+  dnf: boolean;
+  dotd: boolean;
+  fastest_lap: boolean;
+  id: number;
+  position: number;
+  race_distance: number;
+  race_order: number;
+  seasons: { id: number; game: string };
+  pole_position: boolean | undefined;
+  sprint: boolean;
+  teams: { team_name: string };
+  tracks: { distance: number; img: null; layout: null; name: string };
+  users: {
+    country_of_representation: string;
+    first_name: string;
+    human: true;
+    id: number;
+    initials: string;
+    last_name: string;
+    profile_image: string;
+  };
+}
+
+interface Props {
+  trackData: TrackData[];
+}
 
 const AllTime: React.FC<Props> = ({ trackData }) => {
   // Calculate the average position for each user
   const calculateAveragePosition = (userId: number): number => {
-    //@ts-expect-error
     const userData = trackData.filter((data) => data.users.id === userId);
-    //@ts-expect-error
     const sum = userData.reduce((acc, data) => acc + data.position, 0);
     return sum / userData.length;
   };
 
   // Calculate the sum of positions for each user
   const calculateSumOfPositions = (userId: number): number => {
-    //@ts-expect-error
     const userData = trackData.filter((data) => data.users.id === userId);
-    //@ts-expect-error
-    return userData.reduce((acc, data) => acc + positionScore(data.position, data.fastest_lap), 0);
+    return userData.reduce(
+      (acc, data) => acc + positionScore(data.position, data.fastest_lap),
+      0
+    );
+  };
+
+  // Calculate the count of DNF for each user
+  const calculateDNFCount = (userId: number): number => {
+    const userData = trackData.filter((data) => data.users.id === userId);
+    return userData.reduce((acc, data) => acc + (data.dnf ? 1 : 0), 0);
+  };
+
+  // Calculate the count of Pole Position for each user
+  const calculatePolePositionCount = (userId: number): number => {
+    const userData = trackData.filter((data) => data.users.id === userId);
+    return userData.reduce((acc, data) => acc + (data.pole_position ? 1 : 0), 0);
+  };
+
+  // Calculate the count of DOTD for each user
+  const calculateDOTDCount = (userId: number): number => {
+    const userData = trackData.filter((data) => data.users.id === userId);
+    return userData.reduce((acc, data) => acc + (data.dotd ? 1 : 0), 0);
   };
 
   // Extract unique user IDs
-    //@ts-expect-error
-
-  const uniqueUserIds = Array.from(new Set(trackData.map((data) => data.users.id)));
+  const uniqueUserIds = Array.from(
+    new Set(trackData.map((data) => data.users.id))
+  );
 
   // Sort the user IDs by average position in ascending order for the first table
-    //@ts-expect-error
-
-  const sortedUserIdsAverage = uniqueUserIds.sort((a, b) => calculateAveragePosition(a) - calculateAveragePosition(b));
+  const sortedUserIdsAverage = uniqueUserIds.sort(
+    (a, b) => calculateAveragePosition(a) - calculateAveragePosition(b)
+  );
 
   // Sort the user IDs by sum of positions in ascending order for the second table
-    //@ts-expect-error
+  const sortedUserIdsSum = uniqueUserIds.sort(
+    (a, b) => calculateSumOfPositions(b) - calculateSumOfPositions(a)
+  );
 
-  const sortedUserIdsSum = uniqueUserIds.sort((a, b) => calculateSumOfPositions(a) - calculateSumOfPositions(b));
+  // Sort the user IDs by DNF count in descending order for the third table
+  const sortedUserIdsDNF = uniqueUserIds.sort(
+    (a, b) => calculateDNFCount(b) - calculateDNFCount(a)
+  );
+
+  // Sort the user IDs by Pole Position count in descending order for the fourth table
+  const sortedUserIdsPolePosition = uniqueUserIds.sort(
+    (a, b) => calculatePolePositionCount(b) - calculatePolePositionCount(a)
+  );
+
+  // Sort the user IDs by DOTD count in descending order for the fifth table
+  const sortedUserIdsDOTD = uniqueUserIds.sort(
+    (a, b) => calculateDOTDCount(b) - calculateDOTDCount(a)
+  );
 
   // Get the first 10 user IDs for each table
   const first10UserIdsAverage = sortedUserIdsAverage.slice(0, 10);
   const first10UserIdsSum = sortedUserIdsSum.slice(0, 10);
+  const first10UserIdsDNF = sortedUserIdsDNF.slice(0, 5);
+  const first10UserIdsPolePosition = sortedUserIdsPolePosition.slice(0, 5);
+  const first10UserIdsDOTD = sortedUserIdsDOTD.slice(0, 5);
 
   return (
     <div>
@@ -51,15 +113,15 @@ const AllTime: React.FC<Props> = ({ trackData }) => {
         </thead>
         <tbody>
           {first10UserIdsAverage.map((userId) => {
-    //@ts-expect-error
-            const userData = trackData.find((data) => data.users.id === userId);
+            const userData = trackData.find(
+              (data) => data.users.id === userId
+            );
             if (userData) {
               return (
-                //@ts-expect-error
-                
                 <tr key={userId}>
-                  <td>{userData.users.first_name} {userData.users.last_name}</td>
-                  {/*// @ts-ignore */}
+                  <td>
+                    {userData.users.first_name} {userData.users.last_name}
+                  </td>
                   <td>{calculateAveragePosition(userId).toFixed(1)}</td>
                 </tr>
               );
@@ -79,15 +141,100 @@ const AllTime: React.FC<Props> = ({ trackData }) => {
         </thead>
         <tbody>
           {first10UserIdsSum.map((userId) => {
-            //@ts-expect-error
-            const userData = trackData.find((data) => data.users.id === userId);
+            const userData = trackData.find(
+              (data) => data.users.id === userId
+            );
             if (userData) {
               return (
-                //@ts-expect-error
                 <tr key={userId}>
-                  <td>{userData.users.first_name} {userData.users.last_name}</td>
-                  {/*// @ts-ignore */}
+                  <td>
+                    {userData.users.first_name} {userData.users.last_name}
+                  </td>
                   <td>{calculateSumOfPositions(userId)}</td>
+                </tr>
+              );
+            }
+            return null;
+          })}
+        </tbody>
+      </table>
+
+      <h2>DNF (Did Not Finish)</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>DNF Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {first10UserIdsDNF.map((userId) => {
+            const userData = trackData.find(
+              (data) => data.users.id === userId
+            );
+            if (userData) {
+              return (
+                <tr key={userId}>
+                  <td>
+                    {userData.users.first_name} {userData.users.last_name}
+                  </td>
+                  <td>{calculateDNFCount(userId)}</td>
+                </tr>
+              );
+            }
+            return null;
+          })}
+        </tbody>
+      </table>
+
+      <h2>Pole Position</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Pole Position Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {first10UserIdsPolePosition.map((userId) => {
+            const userData = trackData.find(
+              (data) => data.users.id === userId
+            );
+            if (userData) {
+              return (
+                <tr key={userId}>
+                  <td>
+                    {userData.users.first_name} {userData.users.last_name}
+                  </td>
+                  <td>{calculatePolePositionCount(userId)}</td>
+                </tr>
+              );
+            }
+            return null;
+          })}
+        </tbody>
+      </table>
+
+      <h2>DOTD (Driver of the Day)</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>DOTD Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {first10UserIdsDOTD.map((userId) => {
+            const userData = trackData.find(
+              (data) => data.users.id === userId
+            );
+            if (userData) {
+              return (
+                <tr key={userId}>
+                  <td>
+                    {userData.users.first_name} {userData.users.last_name}
+                  </td>
+                  <td>{calculateDOTDCount(userId)}</td>
                 </tr>
               );
             }
@@ -100,4 +247,3 @@ const AllTime: React.FC<Props> = ({ trackData }) => {
 };
 
 export default AllTime;
-
