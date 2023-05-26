630934cda173173410f8c4fab96f1b1e068de82c
Giordano Temple <giordanotemple@gmail.com>
2023-05-25 22:52:32 -0400

    mobile stylings

--- client/src/components/Home/CurrentSeasonStats/Index.tsx
+++ client/src/components/Home/CurrentSeasonStats/Index.tsx
@@ -13,11 +13,11 @@ const CurrentSeasonStats = (props: { seasonData: ObjectType; currentSeason: numb
     <div className='current-season'>
       <h1 className='current-season-title'>Season {props.currentSeason}</h1>
       <div className='current-season-stats'>
-        <div className='current-season-container'>
+        <div>
           <div className='current-season-container-title'>Constructor Standings</div>
           <ConstructorsStandings seasonData={props.seasonData} />
         </div>
-        <div className='current-season-container'>
+        <div>
           <div className='current-season-container-title'>Driver Standings</div>
           <StandingsList seasonData={props.seasonData} />
         </div>
