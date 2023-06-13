0265b6dc8708b4ece25a7b165eecdbecb0bf6c12
Giordano Temple <giordanotemple@gmail.com>
2023-06-13 08:46:26 -0400

    add useTeams hook and update db to include team img

--- server/db/schema/05_create_teams.sql
+++ server/db/schema/05_create_teams.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS teams CASCADE;
 -- CREATE TEAMS
 CREATE TABLE teams (
   id SERIAL PRIMARY KEY,
-  team_name VARCHAR(255) NOT NULL
+  team_name VARCHAR(255) NOT NULL,
+  team_img TEXT
 );
