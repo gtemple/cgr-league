8a7e0e15c8e523e96acfbeefb7e17ee29d8806ea
Giordano Temple <giordanotemple@gmail.com>
2023-06-14 11:00:28 -0400

    add teams

--- client/src/components/Teams/Index.tsx
+++ client/src/components/Teams/Index.tsx
@@ -1,9 +1,22 @@
+import { useParams } from 'react-router-dom'
+import useGetTeam from '../../Hooks/useGetTeam';
+import TeamInfo from './TeamInfo';
 
 
 const Teams = () => {
+  const { id } = useParams();
+  const { teamData = [] } = useGetTeam(id);
+
+  if (!teamData) return null;
+
+
+
   return (
-    <div>Index</div>
+    <div className='container2'>
+      <div className='current-season-title'>Team - {id}</div>
+      <TeamInfo teamData={teamData}/>
+    </div>
   )
 }
 
-export default Teams
+export default Teams;
---
+++ client/src/components/Teams/TeamInfo.tsx
@@ -0,0 +1,45 @@
+interface RaceData {
+  created_at: string;
+  dnf: boolean;
+  dotd: boolean;
+  fastest_lap: boolean;
+  id: number;
+  position: number;
+  pole_position: boolean | null | undefined;
+  race_distance: number;
+  race_order: number;
+  seasons: {
+    id: number;
+  };
+  sprint: boolean;
+  teams: {
+    team_name: string;
+  };
+  tracks: {
+    distance: number;
+    img: string | null;
+    layout: string | null;
+    name: string;
+  };
+  users: {
+    country_of_representation: string;
+    first_name: string;
+    human: boolean;
+    id: number;
+    initials: string;
+    last_name: string;
+    profile_image: string;
+  };
+}
+
+interface SeasonRecordsProps {
+  teamData: RaceData[];
+}
+
+const TeamInfo: React.FC<SeasonRecordsProps> = ({ teamData }) => {
+  return (
+    <div>TeamInfo</div>
+  )
+}
+
+export default TeamInfo
